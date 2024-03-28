9630e4a19b0c33f466e60cbe159c83fb6750300b
Elmanov_Evgeniy <elmanov21@gmail.com>
2024-03-28 17:04:43 +0800

    changed files setting.py and requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8r*x46t+*&2t*t11_97btmk4y-n0te_hbiu(83q7&-vgavj5fa'
+import os
+
+SECRET_KEY = os.getenv('SECRET KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kitchenrecipes.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -82,8 +88,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kitchenrecipes$default',
+        'USER': 'kitchenrecipes',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'kitchenrecipes.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -125,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
